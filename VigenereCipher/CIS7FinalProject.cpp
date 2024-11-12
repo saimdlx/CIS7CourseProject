07471392006ac17639986a0ded447975a42a6cd8
saim <121997985+saimdlx@users.noreply.github.com>
2024-11-11 16:41:00 -0800

    add Vigenere Cipher C++ file.

---
+++ VigenereCipher/CIS7FinalProject.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ *  CIS-7 Final Project: Vigenere Cipher Decryption
+ *  Saim Ahmed, Syed Moiz, Emma Wuysang
+ */
+
+#include <iostream>
+
+//write encryption function (refer to Ei = (Pi + Ki) mod 26
+//write decryption function (refer to Di = (Ei - Ki + 26) mod 26
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
