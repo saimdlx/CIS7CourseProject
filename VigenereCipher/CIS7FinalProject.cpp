996366e6e82d5ff7f32148b4e633ca826306a2fe
saim <121997985+saimdlx@users.noreply.github.com>
2024-11-12 17:54:49 -0800

    Update CIS7FinalProject.cpp
    function headers, "pseudo"code.

--- VigenereCipher/CIS7FinalProject.cpp
+++ VigenereCipher/CIS7FinalProject.cpp
@@ -6,11 +6,20 @@
 #include <iostream>
 
 //write encryption function (refer to Ei = (Pi + Ki) mod 26
+string vigEncrypt(string plaintext, string key){}
 //write decryption function (refer to Di = (Ei - Ki + 26) mod 26
+string vigDecrypt(string ciphertext, string key){}
 
 using namespace std;
 
 int main() {
 
+	//Possible limitations: How long can the plaintext be? Are spaces accounted for by ASCII value? No numbers?
+	
+	//request user input for PLAINTEXT and KEY
+	//throw into vigEncrypt
+	//display encrypted message
+	//throw encrypted message into vigDecrypt
+	
 	return 0;
 }
