0993404413e92429770c519f7507cf49df552ea1
saim <121997985+saimdlx@users.noreply.github.com>
2024-11-30 15:11:50 -0800

    Update CIS7FinalProject.cpp

--- VigenereCipher/CIS7FinalProject.cpp
+++ VigenereCipher/CIS7FinalProject.cpp
@@ -12,13 +12,13 @@ void vigEncrypt(string plaintext, string key){
     string encryptedText;
     int plainNum = 0;
     int keyNum   = 0;
-    char vigChar;
+    char encChar;
     for (int i = 0 ; i < plaintext.size() ; i++) {
 
         plainNum = isupper(plaintext[i]) ? plaintext[i] - 'A' : plaintext[i] - 'a';                             //  i % key.size() is used to avoid out of bounds within the for-loop
         keyNum = isupper(key[i % key.size()]) ? key[i % key.size()] - 'A' : key[i % key.size()] - 'a';          //  ternary operators to determine whether ASCII -> int conversion
-        vigChar = isupper(plaintext[i]) ? ((plainNum + keyNum) % 26) + 'A' : ((plainNum + keyNum) % 26) + 'a';  //  accommodates for upper or lower case.
-        encryptedText.push_back(vigChar);
+        encChar = isupper(plaintext[i]) ? ((plainNum + keyNum) % 26) + 'A' : ((plainNum + keyNum) % 26) + 'a';  //  accommodates for upper or lower case.
+        encryptedText.push_back(encChar);
     }
     cout << encryptedText;
 
@@ -30,15 +30,15 @@ void vigDecrypt(string ciphertext, string key){
 string decryptedText;
 int ciphernum = 0;
 int keyNum = 0;
-char plainChar;
+char decChar;
 
 for (int i = 0; i < ciphertext.size(); i++) {
-   ciphernum = isupper(ciphertext[i]) ? ciphertext[i] - 'A': ciphertext - 'a';
+   ciphernum = isupper(ciphertext[i]) ? ciphertext[i] - 'A': ciphertext[i] - 'a';
    keyNum = isupper(key[i % key.size()]) ? key[i % key.size()] - 'A': key[i % key.size()] - 'a'; 
-   plainChar = isupper(ciphertext[i]) ? ((ciphernum - keyNum +26)% 26) + 'A': ((ciphernum - keyNum + 26)% 26) +'a';
-   decryptedText.push_back(plainChar);  
+   decChar = isupper(ciphertext[i]) ? ((ciphernum - keyNum +26)% 26) + 'A': ((ciphernum - keyNum + 26)% 26) +'a';
+   decryptedText.push_back(decChar);  
     }
-cout << decryptedText;
+    cout << decryptedText;
 }
 
 
